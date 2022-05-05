ea236b06cf9514556da27dc003a02d28b6c9edf5
oliverchang <snxq1995@gmail.com>
2022-05-05 16:58:25 +0800

    chore: refator example code

--- example/main.go
+++ example/main.go
@@ -16,6 +16,8 @@ func main() {
 
 type Handler struct {
 	b wxbot.Bot
+
+	rm *wxbot.RequestMessage
 }
 
 func NewHandler() http.Handler {
@@ -27,27 +29,42 @@ func NewHandler() http.Handler {
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if err := h.handle(r); err != nil {
-		_, _ = w.Write([]byte(err.Error()))
+	var err error
+	defer func() {
+		if err != nil {
+			_, _ = w.Write([]byte(err.Error()))
+		}
+	}()
+
+	h.rm, err = h.requestMsg(r)
+	if err != nil {
+		return
+	}
+
+	err = h.b.SendMsg(h.rm.WebHookURL, h.rm.ChatID, h.handleMsg(), wxbot.Text)
+	if err != nil {
 		return
 	}
 }
 
-func (h *Handler) handle(r *http.Request) (err error) {
+func (h *Handler) requestMsg(r *http.Request) (
+	rm *wxbot.RequestMessage, err error) {
 	br, err := wxbot.Parse(r)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	sign := h.b.GetSign(br.Timestamp, br.Nonce, br.MessageBody.Encrypt)
 	if sign != br.MsgSignature {
-		return fmt.Errorf("签名不一致, sign: %s, got: %s\n", br.MsgSignature, sign)
+		return nil, fmt.Errorf("签名不一致, sign: %s, got: %s", br.MsgSignature, sign)
 	}
-	msg, err := h.b.DecryptRequest(br)
-	if err != nil {
-		return err
-	}
-	content := strings.TrimSpace(strings.TrimPrefix(
-		msg.Text.Content, "@"+h.b.GetFullName()))
+	return h.b.DecryptRequest(br)
+}
 
-	return h.b.SendMsg(msg.WebHookURL, msg.ChatID, content, wxbot.Text)
+func (h *Handler) handleMsg() string {
+	msg := strings.TrimSpace(strings.TrimPrefix(
+		h.rm.Text.Content, "@"+h.b.GetFullName()))
+
+	// 添加消息处理
+
+	return msg
 }
